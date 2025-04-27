b5177e93fe2cb100313f3d989fd665e3a6d0603e
g.balayan <g.balayan@vk.team>
2025-04-28 00:18:19 +0300

    add

--- app/Pythia_parser_RI.py
+++ app/Pythia_parser_RI.py
@@ -1,14 +1,17 @@
+# main.py
 import asyncio
 from consumers.ca_entities_consumer import handle_message
-from rabbitmq_connection import get_channel
+from rabbitmq_connection import get_channel, setup_rabbitmq
 from utils.logger import setup_logger
 import os
 
 logger = setup_logger("main")
-QUEUE_NAME = os.getenv("QUEUE_NAME", "queue_ri_entities")
+QUEUE_NAME = os.getenv("QUEUE_NAME")
+
 async def main():
     channel = await get_channel()
-    queue = await channel.declare_queue(QUEUE_NAME, durable=True)
+    queue = await setup_rabbitmq(channel)
+
     logger.info(f" [*] Waiting for messages in queue '{QUEUE_NAME}'")
     await queue.consume(handle_message)
 
